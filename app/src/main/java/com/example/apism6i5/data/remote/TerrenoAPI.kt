79b387c5f0ec83f7c40d5d9d2cf5fba3dd482d95
Enzo_Gatica <enzogatica96@gmail.com>
2023-08-01 21:53:12 -0400

    se Crea el terreno, TerrenoAPI y TerrenoRetroFit

---
+++ app/src/main/java/com/example/apism6i5/data/remote/TerrenoAPI.kt
@@ -0,0 +1,11 @@
+package com.example.apism6i5.data.remote
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface TerrenoAPI {
+
+    @GET("realestate")
+    suspend fun getData(): Response<List<Terreno>>
+
+}
