626c84a862a259cd90f15b2324aede120562132d
Enzo_Gatica <enzogatica96@gmail.com>
2023-08-01 22:28:53 -0400

    se crea repositorio

---
+++ app/src/main/java/com/example/apism6i5/data/Repositorio.kt
@@ -0,0 +1,13 @@
+package com.example.apism6i5.data
+
+import com.example.apism6i5.data.remote.Terreno
+import com.example.apism6i5.data.remote.TerrenoAPI
+
+class Repositorio(private val terrenoAPI: TerrenoAPI) {
+
+    /*
+    suspend fun cargarTerreno(): List<Terreno>{
+        return terrenoAPI.getData()
+    }
+     */
+}
